6918371e2f0284eaeefc6f37adfe71071c08f3e9
Ana-Morales <moralesanaruth@gmail.com>
2020-09-16 21:19:21 +0000

    Minor changes

--- get_op-func.c
+++ get_op-func.c
@@ -13,7 +13,7 @@ void (*get_op_func(char *ar))(stack_t **stack, unsigned int line)
 		{"pint", op_pint}, {"pop", op_pop}, {"swap", op_swap},
 		{"add", op_add}, {"nop", op_nop}, {"sub", op_sub},
 		{"div", op_div}, {"mul", op_mul}, {"mod", op_mod},
-		{"#", op_nop}, {"pchar", op_pchar}, {"pstr", op_pstr},
+		{"pchar", op_pchar}, {"pstr", op_pstr},
 		{NULL, NULL},
 	};
 	int i = 0;
