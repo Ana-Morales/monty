7b5e1ad91a034101308463f24a54c05f94c7cc49
Ana-Morales <moralesanaruth@gmail.com>
2020-09-16 18:30:43 +0000

    Add # indentifier

--- get_op-func.c
+++ get_op-func.c
@@ -13,7 +13,7 @@ void (*get_op_func(char *ar))(stack_t **stack, unsigned int line)
 		{"pint", op_pint}, {"pop", op_pop}, {"swap", op_swap},
 		{"add", op_add}, {"nop", op_nop}, {"sub", op_sub},
 		{"div", op_div}, {"mul", op_mul}, {"mod", op_mod},
-		{NULL, NULL},
+		{"#", op_nop}, {NULL, NULL},
 	};
 	int i = 0;
 
